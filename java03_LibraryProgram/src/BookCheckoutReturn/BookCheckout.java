c1ca30d19fc089b5329c45ec5d18f8c665f15806
PC <PC@DESKTOP-QPD1M3C>
2021-12-03 13:30:11 +0900

    대출반납 GUI생성

--- java03_LibraryProgram/src/BookCheckoutReturn/BookCheckout.java
+++ java03_LibraryProgram/src/BookCheckoutReturn/BookCheckout.java
@@ -16,10 +16,29 @@ import Book.BookRegister;
 import Book.BookSearch;
 import Member.MemberRegister;
 import Member.MemberSearch;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JRadioButton;
 
 public class BookCheckout extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_9;
+	private JTextField textField_8;
 
 	/**
 	 * Launch the application.
@@ -42,7 +61,7 @@ public class BookCheckout extends JFrame {
 	 */
 	public BookCheckout() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 680, 428);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -117,8 +136,139 @@ public class BookCheckout extends JFrame {
 		mnNewMenu_2.add(mntmNewMenuItem_5);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(306, 35, 67, 89);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel = new JLabel("\uC81C\uBAA9");
+		lblNewLabel.setBounds(385, 35, 57, 15);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_2 = new JLabel("\uC800\uC790");
+		lblNewLabel_2.setBounds(385, 69, 57, 15);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("\uAC00\uACA9");
+		lblNewLabel_3.setBounds(385, 97, 57, 15);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("\uB9C1\uD06C");
+		lblNewLabel_2_1.setBounds(385, 131, 57, 15);
+		contentPane.add(lblNewLabel_2_1);
+		
+		JLabel lblIsbn = new JLabel("ISBN");
+		lblIsbn.setBounds(385, 159, 57, 15);
+		contentPane.add(lblIsbn);
+		
+		JLabel lblNewLabel_2_2 = new JLabel("\uC124\uBA85");
+		lblNewLabel_2_2.setBounds(385, 193, 57, 15);
+		contentPane.add(lblNewLabel_2_2);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(426, 33, 226, 21);
+		contentPane.add(textField);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(426, 64, 226, 21);
+		contentPane.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setEditable(false);
+		textField_2.setColumns(10);
+		textField_2.setBounds(426, 95, 226, 21);
+		contentPane.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setEditable(false);
+		textField_3.setColumns(10);
+		textField_3.setBounds(426, 126, 226, 21);
+		contentPane.add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(426, 157, 226, 21);
+		contentPane.add(textField_4);
+		
+		JButton btnNewButton = new JButton("\uB300\uCD9C");
+		btnNewButton.setBounds(446, 281, 97, 37);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("\uB3C4\uC11C\uAC80\uC0C9");
+		btnNewButton_1.setBounds(555, 281, 97, 37);
+		contentPane.add(btnNewButton_1);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(426, 191, 226, 63);
+		contentPane.add(scrollPane);
+		
+		textField_5 = new JTextField();
+		textField_5.setBounds(428, 191, 224, 61);
+		contentPane.add(textField_5);
+		textField_5.setEditable(false);
+		textField_5.setColumns(10);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("New label");
+		lblNewLabel_1_1.setBounds(12, 35, 67, 89);
+		contentPane.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_4 = new JLabel("\uC774\uB984");
+		lblNewLabel_4.setBounds(91, 35, 57, 15);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_2_3 = new JLabel("\uC0DD\uB144\uC6D4\uC77C");
+		lblNewLabel_2_3.setBounds(91, 69, 57, 15);
+		contentPane.add(lblNewLabel_2_3);
+		
+		JLabel lblNewLabel_3_1 = new JLabel("\uC131\uBCC4");
+		lblNewLabel_3_1.setBounds(91, 101, 57, 15);
+		contentPane.add(lblNewLabel_3_1);
+		
+		JLabel lblNewLabel_2_1_1 = new JLabel("\uC774\uBA54\uC77C");
+		lblNewLabel_2_1_1.setBounds(91, 144, 57, 15);
+		contentPane.add(lblNewLabel_2_1_1);
+		
+		JLabel lblIsbn_1 = new JLabel("\uC804\uD654\uBC88\uD638");
+		lblIsbn_1.setBounds(91, 205, 57, 15);
+		contentPane.add(lblIsbn_1);
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(146, 35, 150, 21);
+		contentPane.add(textField_6);
+		
+		textField_7 = new JTextField();
+		textField_7.setColumns(10);
+		textField_7.setBounds(146, 69, 150, 21);
+		contentPane.add(textField_7);
+		
+		textField_9 = new JTextField();
+		textField_9.setEditable(false);
+		textField_9.setColumns(10);
+		textField_9.setBounds(146, 144, 150, 45);
+		contentPane.add(textField_9);
+		
+		JButton btnNewButton_2 = new JButton("\uD68C\uC6D0\uAC80\uC0C9");
+		btnNewButton_2.setBounds(199, 281, 97, 37);
+		contentPane.add(btnNewButton_2);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(146, 101, 150, 33);
+		contentPane.add(panel);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("\uB0A8\uC131");
+		panel.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("\uC5EC\uC131");
+		panel.add(rdbtnNewRadioButton_1);
+		
+		textField_8 = new JTextField();
+		textField_8.setColumns(10);
+		textField_8.setBounds(146, 199, 150, 21);
+		contentPane.add(textField_8);
 	}
-
 }
--- java03_LibraryProgram/src/BookCheckoutReturn/BookReturn.java
+++ java03_LibraryProgram/src/BookCheckoutReturn/BookReturn.java
@@ -16,10 +16,21 @@ import Book.BookRegister;
 import Book.BookSearch;
 import Member.MemberRegister;
 import Member.MemberSearch;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class BookReturn extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
 
 	/**
 	 * Launch the application.
@@ -42,7 +53,7 @@ public class BookReturn extends JFrame {
 	 */
 	public BookReturn() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 680, 428);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -117,8 +128,97 @@ public class BookReturn extends JFrame {
 		mnNewMenu_2.add(mntmNewMenuItem_5);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(78, 37, 99, 112);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel = new JLabel("\uC81C\uBAA9");
+		lblNewLabel.setBounds(291, 37, 57, 15);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_2 = new JLabel("\uC800\uC790");
+		lblNewLabel_2.setBounds(291, 71, 57, 15);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("\uAC00\uACA9");
+		lblNewLabel_3.setBounds(291, 99, 57, 15);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("\uB9C1\uD06C");
+		lblNewLabel_2_1.setBounds(291, 133, 57, 15);
+		contentPane.add(lblNewLabel_2_1);
+		
+		JLabel lblIsbn = new JLabel("ISBN");
+		lblIsbn.setBounds(291, 161, 57, 15);
+		contentPane.add(lblIsbn);
+		
+		JLabel lblNewLabel_2_2 = new JLabel("\uC124\uBA85");
+		lblNewLabel_2_2.setBounds(291, 195, 57, 15);
+		contentPane.add(lblNewLabel_2_2);
+		
+		JLabel lblNewLabel_3_1 = new JLabel("\uB300\uCD9C\uC790");
+		lblNewLabel_3_1.setBounds(291, 223, 57, 15);
+		contentPane.add(lblNewLabel_3_1);
+		
+		JLabel lblNewLabel_2_1_1 = new JLabel("\uBC18\uB0A9\uC608\uC815\uC77C");
+		lblNewLabel_2_1_1.setBounds(291, 257, 70, 15);
+		contentPane.add(lblNewLabel_2_1_1);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(360, 37, 226, 21);
+		contentPane.add(textField);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(360, 68, 226, 21);
+		contentPane.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setEditable(false);
+		textField_2.setColumns(10);
+		textField_2.setBounds(360, 99, 226, 21);
+		contentPane.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setEditable(false);
+		textField_3.setColumns(10);
+		textField_3.setBounds(360, 130, 226, 21);
+		contentPane.add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(360, 161, 226, 21);
+		contentPane.add(textField_4);
+		
+		textField_5 = new JTextField();
+		textField_5.setEditable(false);
+		textField_5.setColumns(10);
+		textField_5.setBounds(360, 192, 226, 21);
+		contentPane.add(textField_5);
+		
+		textField_6 = new JTextField();
+		textField_6.setEditable(false);
+		textField_6.setColumns(10);
+		textField_6.setBounds(360, 223, 226, 21);
+		contentPane.add(textField_6);
+		
+		textField_7 = new JTextField();
+		textField_7.setEditable(false);
+		textField_7.setColumns(10);
+		textField_7.setBounds(360, 254, 226, 21);
+		contentPane.add(textField_7);
+		
+		JButton btnNewButton = new JButton("\uB3C4\uC11C\uAC80\uC0C9");
+		btnNewButton.setBounds(303, 285, 97, 37);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("\uBC18\uB0A9");
+		btnNewButton_1.setBounds(489, 285, 97, 37);
+		contentPane.add(btnNewButton_1);
 	}
 
 }
