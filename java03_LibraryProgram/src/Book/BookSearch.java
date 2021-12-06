a316e040d9478aa06ff876976eaa78a03ae3c79a
JHLgithuber <88205863+JHLgithuber@users.noreply.github.com>
2021-12-07 00:21:46 +0900

    도서검색 GUI 추가
    도서검색 GUI 초기버전

--- java03_LibraryProgram/src/Book/BookSearch.java
+++ java03_LibraryProgram/src/Book/BookSearch.java
@@ -19,10 +19,28 @@ import Member.MemberSearch;
 import javax.swing.JLabel;
 import java.awt.GridLayout;
 import javax.swing.JSplitPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.GridBagLayout;
+import javax.swing.BoxLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import net.miginfocom.swing.MigLayout;
+import java.awt.FlowLayout;
+import javax.swing.SpringLayout;
 
 public class BookSearch extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	/**
+	 * @wbp.nonvisual location=169,284
+	 */
+	private final JPanel panel_2_5 = new JPanel();
 
 	/**
 	 * Launch the application.
@@ -44,6 +62,25 @@ public class BookSearch extends JFrame {
 	 * Create the frame.
 	 */
 	public BookSearch() {
+		panel_2_5.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblNewLabel_3_5 = new JLabel("image");
+		panel_2_5.add(lblNewLabel_3_5);
+		
+		JLabel lblNewLabel_4_5 = new JLabel("\uC81C\uBAA9:");
+		panel_2_5.add(lblNewLabel_4_5);
+		
+		JLabel lblNewLabel_5_5 = new JLabel("\uC81C\uBAA9\uD544\uB4DC");
+		panel_2_5.add(lblNewLabel_5_5);
+		
+		JLabel lblNewLabel_6_5 = new JLabel("\uC800\uC790:");
+		panel_2_5.add(lblNewLabel_6_5);
+		
+		JLabel lblNewLabel_7_5 = new JLabel("\uC800\uC790\uD544\uB4DC");
+		panel_2_5.add(lblNewLabel_7_5);
+		
+		JButton btnNewButton_1_5 = new JButton("\uC0C1\uC138\uC815\uBCF4");
+		panel_2_5.add(btnNewButton_1_5);
 		setTitle("LibaryManager");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -135,6 +172,146 @@ public class BookSearch extends JFrame {
 		
 		JLabel lblNewLabel = new JLabel("\uB3C4\uC11C\uAC80\uC0C9");
 		splitPane_1.setLeftComponent(lblNewLabel);
+		
+		JPanel panel_1 = new JPanel();
+		splitPane_1.setRightComponent(panel_1);
+		
+		JLabel lblNewLabel_1 = new JLabel("\uC81C\uBAA9:");
+		panel_1.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("\uC800\uC790:l");
+		panel_1.add(lblNewLabel_2);
+		
+		textField_1 = new JTextField();
+		panel_1.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("\uAC80\uC0C9");
+		panel_1.add(btnNewButton);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		splitPane.setRightComponent(scrollPane);
+		
+		JPanel panel = new JPanel();
+		scrollPane.setViewportView(panel);
+		panel.setLayout(new MigLayout("", "[410.00,grow]", "[44.00,grow][grow][grow][grow][grow]"));
+		
+		JPanel panel_2 = new JPanel();
+		panel.add(panel_2, "cell 0 0,growx,aligny center");
+		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblNewLabel_3 = new JLabel("image");
+		panel_2.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("\uC81C\uBAA9:");
+		panel_2.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("\uC81C\uBAA9\uD544\uB4DC");
+		panel_2.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("\uC800\uC790:");
+		panel_2.add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("\uC800\uC790\uD544\uB4DC");
+		panel_2.add(lblNewLabel_7);
+		
+		JButton btnNewButton_1 = new JButton("\uC0C1\uC138\uC815\uBCF4");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panel_2.add(btnNewButton_1);
+		
+		JPanel panel_2_1 = new JPanel();
+		panel.add(panel_2_1, "cell 0 1,grow");
+		panel_2_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblNewLabel_3_1 = new JLabel("image");
+		panel_2_1.add(lblNewLabel_3_1);
+		
+		JLabel lblNewLabel_4_1 = new JLabel("\uC81C\uBAA9:");
+		panel_2_1.add(lblNewLabel_4_1);
+		
+		JLabel lblNewLabel_5_1 = new JLabel("\uC81C\uBAA9\uD544\uB4DC");
+		panel_2_1.add(lblNewLabel_5_1);
+		
+		JLabel lblNewLabel_6_1 = new JLabel("\uC800\uC790:");
+		panel_2_1.add(lblNewLabel_6_1);
+		
+		JLabel lblNewLabel_7_1 = new JLabel("\uC800\uC790\uD544\uB4DC");
+		panel_2_1.add(lblNewLabel_7_1);
+		
+		JButton btnNewButton_1_1 = new JButton("\uC0C1\uC138\uC815\uBCF4");
+		panel_2_1.add(btnNewButton_1_1);
+		
+		JPanel panel_2_2 = new JPanel();
+		panel.add(panel_2_2, "cell 0 2,grow");
+		panel_2_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblNewLabel_3_2 = new JLabel("image");
+		panel_2_2.add(lblNewLabel_3_2);
+		
+		JLabel lblNewLabel_4_2 = new JLabel("\uC81C\uBAA9:");
+		panel_2_2.add(lblNewLabel_4_2);
+		
+		JLabel lblNewLabel_5_2 = new JLabel("\uC81C\uBAA9\uD544\uB4DC");
+		panel_2_2.add(lblNewLabel_5_2);
+		
+		JLabel lblNewLabel_6_2 = new JLabel("\uC800\uC790:");
+		panel_2_2.add(lblNewLabel_6_2);
+		
+		JLabel lblNewLabel_7_2 = new JLabel("\uC800\uC790\uD544\uB4DC");
+		panel_2_2.add(lblNewLabel_7_2);
+		
+		JButton btnNewButton_1_2 = new JButton("\uC0C1\uC138\uC815\uBCF4");
+		panel_2_2.add(btnNewButton_1_2);
+		
+		JPanel panel_2_3 = new JPanel();
+		panel.add(panel_2_3, "cell 0 3,grow");
+		panel_2_3.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblNewLabel_3_3 = new JLabel("image");
+		panel_2_3.add(lblNewLabel_3_3);
+		
+		JLabel lblNewLabel_4_3 = new JLabel("\uC81C\uBAA9:");
+		panel_2_3.add(lblNewLabel_4_3);
+		
+		JLabel lblNewLabel_5_3 = new JLabel("\uC81C\uBAA9\uD544\uB4DC");
+		panel_2_3.add(lblNewLabel_5_3);
+		
+		JLabel lblNewLabel_6_3 = new JLabel("\uC800\uC790:");
+		panel_2_3.add(lblNewLabel_6_3);
+		
+		JLabel lblNewLabel_7_3 = new JLabel("\uC800\uC790\uD544\uB4DC");
+		panel_2_3.add(lblNewLabel_7_3);
+		
+		JButton btnNewButton_1_3 = new JButton("\uC0C1\uC138\uC815\uBCF4");
+		panel_2_3.add(btnNewButton_1_3);
+		
+		JPanel panel_2_4 = new JPanel();
+		panel.add(panel_2_4, "cell 0 4,grow");
+		panel_2_4.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblNewLabel_3_4 = new JLabel("image");
+		panel_2_4.add(lblNewLabel_3_4);
+		
+		JLabel lblNewLabel_4_4 = new JLabel("\uC81C\uBAA9:");
+		panel_2_4.add(lblNewLabel_4_4);
+		
+		JLabel lblNewLabel_5_4 = new JLabel("\uC81C\uBAA9\uD544\uB4DC");
+		panel_2_4.add(lblNewLabel_5_4);
+		
+		JLabel lblNewLabel_6_4 = new JLabel("\uC800\uC790:");
+		panel_2_4.add(lblNewLabel_6_4);
+		
+		JLabel lblNewLabel_7_4 = new JLabel("\uC800\uC790\uD544\uB4DC");
+		panel_2_4.add(lblNewLabel_7_4);
+		
+		JButton btnNewButton_1_4 = new JButton("\uC0C1\uC138\uC815\uBCF4");
+		panel_2_4.add(btnNewButton_1_4);
 	}
-
 }
